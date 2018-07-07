c4d80829e6d61df7a1651eb6113856783f4cf4fe
mldn <mldnqa@163.com>
2018-07-07 10:29:13 +0800

    SprinBoot程序快速启动。

---
+++ micro-boot/src/main/java/cn/mldn/StartBootMain.java
@@ -0,0 +1,11 @@
+package cn.mldn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication 
+public class StartBootMain {
+	public static void main(String[] args) {
+		SpringApplication.run(StartBootMain.class, args) ;
+	}
+}
